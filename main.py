04966312ca8e2748f56d36d225d12edaace24aba
USER <k.nerush@whitetea.tech>
2025-11-11 14:22:57 +0300

    test text

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("пук")
+try:
+    number1 = int(input("Введи число: "))
+    number2 = int(input("И еще раз число: "))
+    result = number1 / number2
+except ValueError:
+    print("Товарищ! Это не число!")
+except ZeroDivisionError:
+    print("Вас в школе не учили, что на ноль делить нельзя??")
+else:
+    print(f"Поделили и получилось: {result}")
+finally:
+    print("ВОТ И ВСЕ! ПОКА!")
