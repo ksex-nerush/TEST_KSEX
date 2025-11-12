bf71efd69d52a3b37d07efa37bb74420e3941783
USER <k.nerush@whitetea.tech>
2025-11-12 11:27:03 +0300

    домашка по экранизирующим последовательностям

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-try:
-    numbers1 = int(input("Введи первое число на сложение: "))
-    numbers2 = int(input("А теперь введи второе число на сложение: "))
-    numbers3 = int(input("Введи первое число на вычитание: "))
-    numbers4 = int(input("А теперь введи второе число на вычитание: "))
-    numbers5 = int(input("Введи первое число на умножение: "))
-    numbers6 = int(input("А теперь введи второе число на умножение: "))
-    numbers7 = int(input("Введи первое число на деление: "))
-    numbers8 = int(input("А теперь введи второе число на деление: "))
-
-    result1 = numbers1 + numbers2
-    result2 = numbers3 - numbers4
-    result3 = numbers5 * numbers6
-    result4 = numbers7 / numbers8
-except ValueError:
-    print("Товарищ! Это не число!")
-except ZeroDivisionError:
-    print("Вас в школе не учили, что на ноль делить нельзя??")
-else:
-    print(f"Сумма сложения: {result1}", f"Сумма вычитания: {result2}", f"Сумма умножения: {result3}", f"Сумма деления: {result4}", sep = "\n")
+double_quotes = "А я ему и говорю, \"Напиши про хаб, никаких скриншотов!!\""
+single_quote = 'А в чем разница между \'двойными\' и \'одинарными\' кавычками?'
+backslash = "Дамп сохранила по C:\\Users\\USER\\"
+new_line = "Владимирский централ, \nВетер северный"
+tab = "Ричард\tХендрикс\tОдобряет"
+multi_line = """Пишу письмо "Менеджерам"
+с большой признательностью
+и уважением \nк вашей\tработе"""
+print(double_quotes, single_quote, backslash, new_line, tab, multi_line, sep='\n')
