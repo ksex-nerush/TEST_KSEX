b4bb6ec9f642c209c4c2262eb01b403cc56587b0
USER <k.nerush@whitetea.tech>
2025-11-12 15:03:41 +0300

    домашка по форматированию строк

---
+++ string_formatting.py
@@ -0,0 +1,8 @@
+name = "Ксения"
+age = 27
+city = "Саратов"
+text_format = "Привет! Меня зовут {}. Мне целых {} лет. Я живу в городе {}".format(name, age, city)
+text_f = f"Привет! Меня зовут {name}. Мне целых {age} лет. Я живу в городе {city}"
+
+print(text_format)
+print(text_f)
