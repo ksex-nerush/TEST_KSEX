8b460dd160a86725040b17ffec8a41d6cc2a4f6a
USER <k.nerush@whitetea.tech>
2025-11-13 16:30:55 +0300

    домашка по функциям с выводом типа и длины объекта

--- function_return.py
+++ function_return.py
@@ -3,6 +3,8 @@ def multiply (a: int, b: int):
         return a * b
     except TypeError as ex:
         return f"ОШИБКА! Аргументы не являются цифрами. Введите числовые значения! Подробности ошибки: {ex}"
+    except Exception as ex:
+        return f"НЕПРЕДВИДИМАЯ ОШИБКА: {ex.__class__}! Подробности ошибки: {ex}"
 
 
 result = multiply(6, 8)
