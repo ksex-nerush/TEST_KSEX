b72b64e50ad137e760298f4e1425f515a886c236
USER <k.nerush@whitetea.tech>
2025-11-13 14:04:18 +0300

    домашка по функциям с выводом типа и длины объекта

--- function_info.py
+++ function_info.py
@@ -1,5 +1,10 @@
 def show_info_object(object):
-    return f"тип {type(object)}, длина {len(object)}"
+    try:
+        return f"тип {type(object)}, длина {len(object)}"
+    except TypeError as ex:
+        return f"ОШИБКА! Объект не может являться цифрой. Введите другой тип значения! Подробности ошибки: {ex}"
+    except Exception as ex:
+        return f"НЕПРЕДВИДИМАЯ ОШИБКА: {ex.__class__}! Подробности ошибки: {ex}"
 
 
 print(show_info_object("строка"))
