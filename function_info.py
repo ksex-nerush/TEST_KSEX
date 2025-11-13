e120f5ac77f253dc651204342ef5d1fdc39bfd33
USER <k.nerush@whitetea.tech>
2025-11-13 13:36:00 +0300

    домашка по функциям

--- function_info.py
+++ function_info.py
@@ -1,6 +1,7 @@
 def show_info_object(object):
     return f"тип {type(object)}, длина {len(object)}"
 
+
 print(show_info_object("строка"))
 print(show_info_object([2, 5, 7, 7]))
 print(show_info_object((2, 5, 7, 7)))
--- function_return.py
+++ function_return.py
@@ -1,5 +1,15 @@
-def multiply (a, b):
-    return a * b
+def multiply (a: int, b: int):
+    try:
+        return a * b
+    except TypeError as ex:
+        return f"ОШИБКА! Аргументы не являются цифрами. Введите числовые значения! Подробности ошибки: {ex}"
+    except Exception as ex:
+        return f"НЕПРЕДВИДИМАЯ ОШИБКА: {ex.__class__}! Подробности ошибки: {ex}"
 
-result = multiply(6, 7)
+
+result = multiply(6, 8)
+print(result)
+result = multiply("dwdwdwd", "mmmmmm")
+print(result)
+result = multiply("6", 8)
 print(result)
